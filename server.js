72857fbbf70026721232536712ed5859e322ab4e
YurGoV <yurgov@gmail.com>
2023-02-01 10:45:36 +0200

    current

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const morgan = require('morgan')
 const app = express();
 
+// todo: 1.2 // 1:52:20 - розгорнути простий веб-сервер
+// 1:57:0 - middleware morgan
+
 const PORT = 8081;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));//обробка стандартних даних хтмл-форм ?? deprecated
+app.use(express.static('public'));// дозволяє зробити директорію публічною
+app.use(morgan('tiny'));
 
 // app.get('/home', (request, response) => {
 //     response.sendStatus(200);
 // })
+
+/*app.use( (request, response, next) => {
+    // response.send('middleware request');
+    // response.status(500).json({status: '500 ))'})
+    console.log(`${request.method} ${request.originalUrl} ${new Date().toISOString()}`);
+    next();
+})*/
 
 
 
@@ -19,13 +35,22 @@ app.get('/home', (request, response) => {
     ])
 })
 
-app.post('/home', (request, response) => {
-    response.send('post request');
+app.post('/home', (req, res) => {
+    // response.send('post request');
+    if(!req.body.id) {
+        res.status(400).json({status: 'id parametr is required'})//валідація запиту 1:55
+    }
+    console.log(req.body);
+    res.json({javascript: 'object', body: req.body})
+    const {id} = req.body;
+    console.log(id);
+
 })
 
 app.delete('/home', (request, response) => {
     response.send('delete request');
 })
+
 
 
 // app.get('/home' - це routing
@@ -35,10 +60,12 @@ app.delete('/home', (request, response) => {
     response.status(500).json({status: '500 ))'})
 }*/
 
-app.use( (request, response) => {
+/*app.use( (request, response, next) => {
     // response.send('middleware request');
-    response.status(500).json({status: '500 ))'})
-})
+    // response.status(500).json({status: '500 ))'})
+    console.log(`${request.method} ${request.originalUrl} ${new Date().toISOString()}`);
+    next()
+})*/
 
 app.listen(PORT, (err) => {
     if (err) {
