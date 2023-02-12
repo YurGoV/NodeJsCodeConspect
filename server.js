a8f44ad2cce313c2b040af652b2ca151a417b4ec
YurGoV <yurgov@gmail.com>
2023-02-12 23:44:38 +0200

    curr

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
 const morgan = require('morgan');
 const app = express();
 
@@ -7,6 +8,11 @@ require('dotenv').config()
 
 
 const {postsRouter} = require('./src/routers/postsRourter')
+
+const url = 'mongodb://127.0.0.1:27017';
+const client = new MongoClient(url);
+const dbName = 'testGoITdb';
+
 
 // process.ENV
 
@@ -19,11 +25,36 @@ app.use(morgan('tiny'));
 app.use('/api/posts', postsRouter)
 
 
-app.listen(PORT, (err) => {
+/*app.listen(PORT, (err) => {
     if (err) {
         console.error('Error at server launch:', err);
     }
     console.log(`Server works at port ${PORT}`);
-});
+});*/
+
+const start = async () => {
+
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const PostsCollection = db.collection('posts');
+
+    // const addPost =  await PostsCollection.insertOne({topic: "toptop2", text: "textex2"})
+    // await PostsCollection.insertOne({topic: "toptop3", text: "textex3"})
 
 
+    const posts = await PostsCollection.find({}).toArray();
+    console.log(posts);
+
+    app.listen(PORT, (err) => {
+        if (err) {
+            console.error('Error at server launch:', err);
+        }
+        console.log(`Server works at port ${PORT}`);
+    });
+}
+
+start()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
